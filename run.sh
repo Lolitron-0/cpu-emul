4bd5d0222c02165f9118f63e72e82c85a934c164
Lolitron-0 <ovcharov_05@mail.ru>
2024-03-07 19:04:37 +0300

    nix env

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-nix develop
-pushd build
-sudo cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G Ninja .. && sudo cmake --build . --parallel 5 && cp compile_commands.json ../compile_commands.json && ./$1 ${@:2}
-popd
+
+set -e
+
+mkdir -p build
+
+nix flake update
+#nix develop
+
+
+sudo cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G Ninja -B build -S . 
+sudo cmake --build build --parallel 5 
+
+cp build/compile_commands.json ./compile_commands.json 
+build/$1 ${@:2}
